0f83da387f5702b0511b7c177329b444681b7ad3
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2022-11-28 14:54:24 +0000

    Check if a list has a circle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: Pointer to the list
+ *
+ * Return: 0 if no cycle, otherwise 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+
+	while (current && current->next != NULL)
+	{
+		current = current->next;
+		if (current == list)
+			return (1);
+	}
+	return (0);
+}
